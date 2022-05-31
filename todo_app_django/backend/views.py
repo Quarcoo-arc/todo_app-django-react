6f63683402151bfb2d0f69fe014c5a014b43a37f
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-05-31 18:38:35 +0000

    TaskViewSet added

--- todo_app_django/backend/views.py
+++ todo_app_django/backend/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from models import Task
+from rest_framework import viewsets, permissions
+from todo_app_django.backend.serializers import TaskSerializer
 
 # Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows tasks to be viewed or edited
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
