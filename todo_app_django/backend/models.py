7b3d8291682989f3e30079986f456201d9bc36ba
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-05-31 18:15:25 +0000

    Task model added

--- todo_app_django/backend/models.py
+++ todo_app_django/backend/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    item = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
