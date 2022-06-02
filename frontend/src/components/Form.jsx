7f9ec877ad928b18441d8af96e53c8edbe860937
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-06-02 10:35:56 +0000

    delete task functionality added

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { ReactComponent as PlusIcon } from "../assets/svgs/PlusIcon.svg";
+import { ReactComponent as EditIcon } from "../assets/svgs/EditIcon.svg";
+import { ReactComponent as DeleteIcon } from "../assets/svgs/DeleteIcon.svg";
 
 const Form = () => {
   const [newTask, setNewTask] = useState("");
@@ -27,7 +29,7 @@ const Form = () => {
       return alert("Please enter a task!");
     }
     try {
-      fetch("http://127.0.0.1:8000/tasks/", {
+      const recentTask = await fetch("http://127.0.0.1:8000/tasks/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -36,11 +38,21 @@ const Form = () => {
           item: newTask,
         }),
       });
+      const data = await recentTask.json();
+      data && setTaskList((prev) => [...prev, data]);
     } catch (error) {
       console.log(error.message);
     }
-    setTaskList((prev) => [...prev, { item: newTask }]);
     setNewTask("");
+  };
+
+  const deleteTask = (url) => {
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => setTaskList(taskList.filter((item) => item.url !== url)));
   };
 
   return (
@@ -49,6 +61,20 @@ const Form = () => {
         <div key={index} className="task">
           <input type="checkbox" name={"task" + index} id={"task" + index} />
           <label htmlFor={"task" + index}>{item.item}</label>
+          <div className="icons">
+            <EditIcon
+              width="1.5rem"
+              className="icon"
+              fill="#e1e104"
+              onClick={() => console.log(index)}
+            />
+            <DeleteIcon
+              width="1.5rem"
+              className="icon"
+              fill="#d20707"
+              onClick={deleteTask.bind(null, item.url)}
+            />
+          </div>
         </div>
       ))}
       <div className="task">
