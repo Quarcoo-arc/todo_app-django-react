e9cfdb87c2a8579d6ec24f7bad0ad8f3e4f1401b
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-06-02 12:23:22 +0000

    Footer component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import Form from "./components/Form";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="container">
       <h1 className="title">{new Date().toDateString()}</h1>
       <Form />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      &copy;{new Date().getFullYear()} |{" "}
+      <a href="https://github.com/Quarcoo-arc" target="_blank" rel="noreferrer">
+        Minoq 😇
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
