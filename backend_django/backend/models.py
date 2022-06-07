d8c9424e1b6035a947b78408186220e5606bbf77
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-06-07 22:13:21 +0000

    Added user foreign key to task model

--- backend_django/backend/models.py
+++ backend_django/backend/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class Task(models.Model):
     item = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('auth.User', related_name='tasks', on_delete=models.CASCADE)
