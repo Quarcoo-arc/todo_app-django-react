f509ae2b96332b7febaf587509e318ddacce41d5
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-06-08 06:29:50 +0000

    UserSerializer class added

--- backend_django/backend/serializers.py
+++ backend_django/backend/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from backend.models import Task
+from django.contrib.auth.models import User
 
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Task
         fields = "__all__"
+
+class UserSerializer(serializers.ModelSerializer):
+    tasks = serializers.PrimaryKeyRelatedField(many=True, querySet=Task.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'tasks']
